44020e8bb60f44a84113813ffceaf1e0442b1571
Joseph Adediji <josylado@gmail.com>
2019-09-18 17:02:30 +0300

    Update template and css

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from app.config import Config
 from flask_migrate import Migrate
 from app.config import config_options
 from flask_mail import Mail
+from app import error
 
 
 login_manager = LoginManager()
