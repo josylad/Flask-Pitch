76295a78def0e7e93c8bd2e2611ea2fb5272a0be
Joseph Adediji <josylado@gmail.com>
2019-09-18 15:29:19 +0300

    Add gitignore file

---
+++ run.py
@@ -0,0 +1,19 @@
+from app import create_app, db
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager, Server
+from app.models import User, Post
+
+app = create_app()
+manager = Manager(app)
+migrate = Migrate(app,db)
+manager.add_command('runserver', Server)
+manager.add_command('db', MigrateCommand)
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app, db = db, User    = User )
+
+if __name__ == '__main__':
+    manager.run()
+    
