ea4ae7f917a06001a1d240119b0dc2f7b27dc5e7
Joseph Adediji <josylado@gmail.com>
2019-09-18 15:55:49 +0300

    Add gmail email config

--- app/config.py
+++ app/config.py
@@ -5,9 +5,14 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://josylad:p@localhost/pitchy'
     
-)
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     
-
 
 class ProdConfig(Config):
     '''
